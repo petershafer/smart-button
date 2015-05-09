b77b5d7a6095a3f4c1fb63183245601949902b19
Peter Shafer <peter.shafer@tivix.com>
2015-05-09 09:16:45 -0700

    Simplifying polymer example and adding support for custom event firing instead of binding promises.  Binding data with polymer is perfectly fine, but outside the spec I'm trying to implement these components with.

--- polymer/app/scripts/app.js
+++ polymer/app/scripts/app.js
@@ -4,6 +4,40 @@
   document.addEventListener('polymer-ready', function() {
     // Perform some behaviour
     console.log('Polymer is ready to rock!');
+
+	var delay = function(ms) {
+        var deferred = Q.defer();
+        setTimeout(deferred.resolve, ms);
+        return deferred.promise;
+    }
+
+    var fail = function(ms) {
+        var deferred = Q.defer();
+        setTimeout(deferred.reject, ms);
+        return deferred.promise;
+    }
+
+    document.getElementsByTagName("body")[0].addEventListener('hello', function(e){
+        delay(1000).then(function(){
+        	e.target.dispatchEvent(new Event('hello.success'));
+        }).catch(function(){
+        	e.target.dispatchEvent(new Event('hello.error'));
+        }).done(function(){
+        	e.target.dispatchEvent(new Event('hello.done'));
+        });
+    });
+
+    document.getElementsByTagName("body")[0].addEventListener('world', function(e){
+        fail(1000).then(function(){
+        	e.target.dispatchEvent(new Event('world.success'));
+        }).catch(function(){
+        	e.target.dispatchEvent(new Event('world.error'));
+        }).done(function(){
+        	e.target.dispatchEvent(new Event('world.done'));
+        });
+    });
+
+
   });
 
 // wrap document so it plays nice with other libraries
