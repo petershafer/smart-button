ba9842676a31250c7e33e5a97fa57890802644ab
Peter Shafer <peter.shafer@tivix.com>
2015-05-09 07:08:03 -0700

    Removing event listeners on destruction and change for x-tag example.

--- x-tag/app/scripts/smart-button.js
+++ x-tag/app/scripts/smart-button.js
@@ -11,43 +11,55 @@
         lifecycle: {
             // Fires when an instance of the element is created
             created: function() {
-                this.done = false;
-                this.retry = false;
-                this.error = false;
-                var a = this.firstElementChild;
-                a.innerHTML = this.getAttribute("label");
-                this.retry = this.getAttribute("allow-retry") !== null;
-                a.className = baseClasses + successClass;
-
-                var self = this;
-                var a = this.firstElementChild;
-                this.addEventListener(this.getAttribute("activate") + ".success", function(){
-                    a.innerHTML = self.getAttribute("complete-label") || self.getAttribute("label");
+                this.initialize = function(){
+                    this.done = false;
+                    this.retry = false;
+                    this.error = false;
+                    var a = this.firstElementChild;
+                    a.innerHTML = this.getAttribute("label");
+                    this.retry = this.getAttribute("allow-retry") !== null;
                     a.className = baseClasses + successClass;
-                    self.done = true;
-                });
 
-                this.addEventListener(this.getAttribute("activate") + ".error", function(){
-                    a.className = baseClasses + failClass;
-                    self.error = true;
-                    a.innerHTML = self.getAttribute("failure-label") || self.getAttribute("label");
-                    self.done = false;          
-                });
-                
-                this.addEventListener(this.getAttribute("activate") + ".done", function(){
-                    a.removeAttribute("disabled");        
-                });
-
+                    var self = this;
+                    var a = this.firstElementChild;
+                    this.endSuccess = function(){
+                        a.innerHTML = self.getAttribute("complete-label") || self.getAttribute("label");
+                        a.className = baseClasses + successClass;
+                        self.done = true;
+                    }
+                    this.endError = function(){
+                        a.className = baseClasses + failClass;
+                        self.error = true;
+                        a.innerHTML = self.getAttribute("failure-label") || self.getAttribute("label");
+                        self.done = false;          
+                    }
+                    this.endDone = function(){
+                        a.removeAttribute("disabled");        
+                    }
+                    this.addEventListener(this.getAttribute("activate") + ".success", this.endSuccess);
+                    this.addEventListener(this.getAttribute("activate") + ".error", this.endError);
+                    this.addEventListener(this.getAttribute("activate") + ".done", this.endDone);
+                }
+                this.initialize();
             },
 
             // Fires when an instance was inserted into the document
             inserted: function() {},
 
             // Fires when an instance was removed from the document
-            removed: function() {},
+            removed: function() {
+                this.removeEventListener(this.getAttribute("activate") + ".success", this.endSuccess);
+                this.removeEventListener(this.getAttribute("activate") + ".error", this.endError);
+                this.removeEventListener(this.getAttribute("activate") + ".done", this.endDone);
+            },
 
             // Fires when an attribute was added, removed, or updated
-            attributeChanged: function(attr, oldVal, newVal) {}
+            attributeChanged: function(attr, oldVal, newVal) {
+                this.removeEventListener(this.getAttribute("activate") + ".success", this.endSuccess);
+                this.removeEventListener(this.getAttribute("activate") + ".error", this.endError);
+                this.removeEventListener(this.getAttribute("activate") + ".done", this.endDone);
+                this.initialize();
+            }
         },
         events: {
             'tap': function(){
